11085db0dbd9f994854585e4a132f7dea38b82c0
Santiago <84828241+astrocinco@users.noreply.github.com>
2023-10-08 23:22:55 -0300

    Changed: mejora de solubilidad de sets.py

--- code/sets.py
+++ code/sets.py
@@ -1,20 +1,24 @@
 MAX_EFFORT = 2000
 MAX_ENERGY = 2000
 
+
+
 def get_data_set_from_file(file):
-    e = []
-    s = []
+    effort_list = []
+    energy_list = []
     with open(file,'r') as o_file:
         i=1
         size = int(o_file.readline())
         for line in o_file:
             if(i <= size):
-                e.append(int(line))
+                effort_list.append(int(line))
             else:
-                s.append(int(line))
+                energy_list.append(int(line))
             i+=1
             
-    return (e, s)
+    return (effort_list, energy_list)
+
+
 
 def get_solution_from_file(file, set):
     set_founded = False
@@ -38,32 +42,33 @@ def get_solution_from_file(file, set):
     return (optimal, secuence)
 
 
+
 def create_random_data_set(days, max_effort = MAX_EFFORT, max_energy = MAX_ENERGY, min_effort = 0 , min_energy = 0):
     import random as r
-    e = []
-    s = []
+    effort_list = []
+    energy_list = []
     
     if max_energy < days:
         print("set invalido")
-        return (e,s)
-    for i in range(0,days):
-        e.append(r.randint(min_effort, max_effort))
+        return (effort_list,energy_list)
+    for i in range(0, days):
+        effort_list.append(r.randint(min_effort, max_effort))
         
         if i == 0:
-            s.append( r.randint(min_energy + days, max_energy))
+            energy_list.append( r.randint(min_energy + days, max_energy))
         else:
             try:
-                s.append(r.randint(min_energy + days-i, s[i-1]-1)) 
+                energy_list.append(r.randint(min_energy + days-i, energy_list[i-1]-1)) 
             except ValueError:
-                print(f"VALUE ERROR: minimo = {min_energy + days-i}, maximo = {s[i-1]-1}")
+                print(f"VALUE ERROR: minimo = {min_energy + days-i}, maximo = {energy_list[i-1]-1}")
                 print(f"min_energy = {min_energy}, dias menos i = {days-i}, i = {i}")
 
-    return (e,s)
+    return (effort_list, energy_list)
+
+
 
 def data_set_to_txt(data_set : tuple, n, file_name):
-    
     file_name = "2c2023-TDA-TP2\data\personalized_sets\_" + file_name 
-    
     file = open(file_name,"w")
     file.write(str(n)+"\n")
     
@@ -75,13 +80,3 @@ def data_set_to_txt(data_set : tuple, n, file_name):
     file.close()
     
     return 0
-
-'''
-print(get_data_set_from_file('data/3.txt'))
-print(get_solution_from_file('data/Resultados Esperados.txt', '10.txt'))
-
-
-data = create_random_data_set(5,25,30)
-data_set_to_txt(data, 5, "prueba.txt")
-'''
-
